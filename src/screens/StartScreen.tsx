f519f37057b12db3c1c5482958ba1e9b4f3f8240
Sarec21 <129178034+Sarec21@users.noreply.github.com>
2025-06-18 14:37:10 +0200

    Improve start screen title styling

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -7,7 +7,7 @@ export default function StartScreen() {
   return (
     <div className="start-screen">
       <div className="content">
-        <h1>{t('game_title')}</h1>
+        <h1 className="title">{t('game_title')}</h1>
         <button onClick={goToLevelSelect}>{t('start_game')}</button>
       </div>
     </div>
